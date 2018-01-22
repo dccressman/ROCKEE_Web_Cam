99415fa2ed8b301304b8db5ec7343b4798c640b4
DCCRESSMAN <dcc58@zips.uakron.edu>
2018-01-22 11:38:52 -0500

    Finds largest contour, then we are using that to try to crop out part of the image so that we can hopefully get more definition.  Tried the rectangle approx of the biggest contour, but too limited. Going to try some polygon models of the contour to pick a better representation and cropping.

--- OpenCV3-Practice/Source.cpp
+++ OpenCV3-Practice/Source.cpp
@@ -1,3 +1,4 @@
+
 // OpenCV3-Practice.cpp : Defines the entry point for the console application.
 
 
@@ -10,6 +11,7 @@
 #include "C:\Users\Dana\Desktop\OpenCV3\opencv\build\include\opencv2\imgproc\imgproc.hpp"
 #include "C:\Users\Dana\Desktop\OpenCV3\opencv\build\include\opencv2\features2d.hpp"
 
+//#include "Adapt.h"
 
 using namespace std;
 using namespace cv;
@@ -21,6 +23,7 @@ int theObject[2] = { 0,0 };
 Mat edges;
 int minArea = 1000;
 int minDistance = 1000;
+
 void SortKeypoints(vector<KeyPoint> &keypoints, Mat &maskedI, Mat &picture, vector<Point3f> &midpoints, vector<float> slope)
 {
 	int k = 0;
@@ -71,8 +74,25 @@ void SortKeypoints(vector<KeyPoint> &keypoints, Mat &maskedI, Mat &picture, vect
 	imshow("blacky", picture);
 
 }
-void preferredOrdering(vector<Point3f> &midpoints, vector<float> slope)
+void preferredOrdering(vector<Point3f> &midpoints, vector<float> &slope, Mat &Again)
 {
+	for (int k = 0; k < midpoints.size()-2; k++)
+	{
+// basic sort, maybe wants to be fancier, but speed is less of an issue than storage is
+		if (slope[k] < slope[k + 1])
+		{
+			float temp;
+			Point3f tempPoint;
+			tempPoint = midpoints[k];
+			midpoints[k] = midpoints[k + 1];
+			midpoints[k + 1] = tempPoint;
+			temp = slope[k];
+			slope[k] = slope[k + 1];
+			slope[k + 1] = temp;
+		}
+	}
+	circle(Again, Point2f(midpoints[0].x, midpoints[0].y), 2, (0, 0, 255), 3);
+	imshow("Preferred Spot", Again);
 	//need to take the slopes and compare to find the biggest slope and then put that point as the preferred point. maybe incorporate
 	//this into the z point for the weight... maybe trade that out for the slope for the weight, though it is kind of nice to have the 
 	// width of what space the robot has to traavel through. some kinds of percentages comaprison
@@ -101,15 +121,20 @@ void pickFeatures(Mat bwdifferential, Mat &picture)
 	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
 	vector<KeyPoint> keypoints;
 	detector->detect(bwdifferential, keypoints);
-	Mat maskedI;
+	Mat maskedI,Again;
 	//create a black image for just the keypoints
 	bwdifferential.copyTo(maskedI);
+	picture.copyTo(Again);
 	threshold(maskedI, maskedI, 0, 0, THRESH_BINARY);//black image
 	//DEBUG
 	cvtColor(maskedI, maskedI, CV_GRAY2BGR);//lets colors be put onto it, will be able to cut out eventually
 	midpoints.resize(keypoints.size()*keypoints.size());
 	slope.resize(keypoints.size()*keypoints.size());
 	SortKeypoints(keypoints, maskedI, picture,midpoints, slope);
+	preferredOrdering(midpoints,slope,Again);
+	
+	
+	imshow("blacky2", picture);
 }
 
 void motionSearch(Mat bwdifferential, Mat &frame1)
@@ -177,9 +202,13 @@ int main()
 	VideoCapture video;
 	int max_thresh = 255;
 	int threshold1 = 0, threshold2 = 0, threshold3 = 0, threshold4 = 0, threshold5 = 0, threshold6 = 0;
+	threshold1 = 220000;
+	threshold2 = 600000;
 	Mat medianBlurred, OrangeCrop, OrangeCrop2;
 	while (1)
 	{
+		vector<Vec4i> hierarchy;
+		vector< vector<Point> > contours;
 		//video.open("RMC.avi");
 		//if (!video.isOpened()) {
 		//	cout << "ERROR ACQUIRING VIDEO FEED\n";
@@ -246,34 +275,104 @@ int main()
 		//thresholding the RGB image
 		imshow("Orange3", OrangeCrop);*/
 
-		video.open("dirts.avi");
+	/*	video.open("dirts.avi");
 		if (!video.isOpened()) {
 			cout << "ERROR ACQUIRING VIDEO FEED\n";
 			getchar();
 			return -1;
 		}
-		if (video.read(frame1))
+		if (video.read(frame1))*/
+		frame1 = imread("8.jpg");
+		if (1)
 		{
 			// height =240
 			// width = 320
-			threshold1 = 22;
-			threshold2 = 80;
 
+			Mat gray3;
 			cvtColor(frame1, gray1, CV_RGB2GRAY);
+			gray1.copyTo(gray3);
+			//Rect roi = Rect(threshold1, threshold2, 298, (frame1.size().height - threshold2));
+			//gray1 = gray1(roi);
+			//threshold(gray1, gray1, 40, 255, THRESH_BINARY);//111
+			adaptiveThreshold(gray3, medianBlurred, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 131, 2);
+			adaptiveThreshold(gray3, gray2, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 67, 2);
 			Rect roi = Rect(threshold1, threshold2, 298, (frame1.size().height - threshold2));
-			gray1 = gray1(roi);
-
-			//threshold(gray1, gray2, 106, 255, THRESH_BINARY);//111
-			adaptiveThreshold(gray1, medianBlurred, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 131, 2);
-			adaptiveThreshold(gray1, gray2, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 67, 2);
-			imshow("Frame1", frame1);
-			imshow("Gray", gray1);
+			//gray1 = gray1(roi);
+			cvtColor(gray2, gray2, CV_GRAY2BGR);
+			cvtColor(gray2, gray2, CV_BGR2GRAY);
+			//findContours(gray3, gray3,hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+			//pickFeatures(medianBlurred, gray3);
+			//imshow("Frame1", frame1);
+			//imshow("Gray", gray1);
 			//motionSearch(gray2, gray1);
-			imshow("BW", gray2);
-			imshow("AdaptiveThreshold", medianBlurred);
-			pickFeatures(medianBlurred, gray1);
+			//imshow("BW", gray2);
+			//imshow("AdaptiveThreshold", medianBlurred);
+			//pickFeatures(medianBlurred, gray3);
+			Mat image;
+			gray2.copyTo(image);
+			//int threshold1 = 0, threshold2 = 0, threshold3 = 0, threshold4 = 0, threshold5 = 0, threshold6 = 0;
+	/*		char TrackbarName3[50], TrackbarName32[50], TrackbarName31[50], TrackbarName34[50], TrackbarName35[50], TrackbarName36[50];
+			sprintf(TrackbarName3, "1L %i", 1000000);
+			sprintf(TrackbarName32, "2L %i", 1000000);
+			sprintf(TrackbarName31, "3L %i", 255);
+			sprintf(TrackbarName34, "1H %i", 255);
+			sprintf(TrackbarName35, "2H %i", 255);
+			sprintf(TrackbarName36, "3H %i", 255);
+			namedWindow("Trackbars", WINDOW_GUI_EXPANDED);
+			createTrackbar(TrackbarName3, "Trackbars", &threshold1, 600000);
+			createTrackbar(TrackbarName32, "Trackbars", &threshold2, 600000);
+			createTrackbar(TrackbarName31, "Trackbars", &threshold3, 255);
+			createTrackbar(TrackbarName34, "Trackbars", &threshold4, 255);
+			createTrackbar(TrackbarName35, "Trackbars", &threshold5, 255);
+			createTrackbar(TrackbarName36, "Trackbars", &threshold6, 255);*/
+
+			vector< vector<Point> > contours;
+			vector<Point> polygon;
+			vector<Vec4i> hierarchy;
+			int areaMin = 21000;
+			int num = 2;
+			int Lc;
+			while (num != 1)
+			{
+				areaMin -= 100;
+				num = 0;
+				findContours(image, contours, hierarchy, CV_RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+				for (int i = 1;i < contours.size(); i++) {
+					if (contourArea(contours.at(i))>areaMin) {
+						drawContours(gray3, contours, i, Scalar(255, 255, 0), 1, 8, hierarchy, 0, Point(0, 0));
+						num++;
+						Lc = i;
+					}
+				}
+			}
+			//rectangle makes it too big of a blocked area
+			//Rect topCut =boundingRect(contours[Lc]);
+			//rectangle(image, topCut, Scalar(255, 255, 255), -1);
+			approxPolyDP(contours[Lc], polygon, 100, true);
+			fillPoly(image, polygon, Scalar(255, 255, 255));
 
 
+			//parameters for the detectro to detect keypoints within the bwdifferential
+			//SimpleBlobDetector::Params params;
+			////params.minThreshold = 10;
+			////params.maxThreshold = 200;
+			//params.filterByArea = true;
+			//params.minArea = 200000;//threshold1;
+			//params.maxArea = 20000000;//threshold2;
+			////params.minDistBetweenBlobs =  minDistance;
+			//params.filterByCircularity = false;
+			//params.filterByColor = false;
+			//params.filterByConvexity = false;
+			////params.minConvexity = 0.8;
+			//params.filterByInertia = true;
+			//params.minInertiaRatio = float(0.05);
+			////detect the keypoints within bwdifferential
+			//Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
+			//vector<KeyPoint> keypoints;
+			//detector->detect(image, keypoints);
+			//drawKeypoints(image, keypoints, image, Scalar(255, 0, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+			imshow("imshowing", gray3);
+			imshow("blockedOut", image);
 		}
 
 		switch (waitKey(10)) {
